e82685c2e9c34be3b504721de2c2f257710f21c4
Eric Schmitt <ericschmitt1@gmail.com>
2016-05-30 09:07:56 -0400

    Add tests and implementation for reducer().

---
+++ test/reducer-spec.js
@@ -0,0 +1,47 @@
+import {Map, fromJS} from 'immutable';
+import {assert} from 'chai';
+import reducer from '../src/reducer';
+
+describe('reducer', () => {
+  it('handles SET_ENTRIES', () => {
+    const state = Map();
+    const entries = ['Trainspotting'];
+    const action = {type: 'SET_ENTRIES', entries: entries};
+    const nextState = reducer(state, action);
+    const expected = fromJS({entries: entries})
+
+    assert.equal(nextState, expected); 
+  });
+
+  it('handles NEXT', () => {
+    const entries = ['Trainspotting', '28 Days Later'];
+    const state = fromJS({entries: entries});
+    const action = {type: 'NEXT'};
+    const nextState = reducer(state, action);
+    const expected = fromJS({
+      vote: { pair: entries }
+    , entries: []
+    });
+
+    assert.equal(nextState, expected);
+  });
+
+  it('handles VOTE', () => {
+    const pair = ['Trainspotting', '28 Days Later'];
+    const state = fromJS({
+      vote: { pair: pair }
+    , entries: []
+    });
+    const action = {type: 'VOTE', entry: 'Trainspotting'};
+    const nextState = reducer(state, action);
+    const expected = fromJS({
+      vote: {
+        pair: pair
+      , tally: { 'Trainspotting': 1 }
+      }
+    , entries: []
+    })
+
+    assert.equal(nextState, expected);
+  });
+});
