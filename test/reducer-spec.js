1ac99ede06e79968ddc35e18f2c4f8676af63000
Eric Schmitt <ericschmitt1@gmail.com>
2016-05-30 23:58:40 -0400

    Add test to verify that reducer works with a reduce method of a list.

--- test/reducer-spec.js
+++ test/reducer-spec.js
@@ -44,4 +44,19 @@ describe('reducer', () => {
 
     assert.equal(nextState, expected);
   });
+
+  it('can be used with reduce', () => {
+    const actions = [
+      {type: 'SET_ENTRIES', entries: ['Trainspotting', '28 Days Later']}
+    , {type: 'NEXT'}
+    , {type: 'VOTE', entry: 'Trainspotting'}
+    , {type: 'VOTE', entry: '28 Days Later'}
+    , {type: 'VOTE', entry: 'Trainspotting'}
+    , {type: 'NEXT'}
+    ];
+    const finalState = actions.reduce(reducer, Map());
+    const expected = Map({ winner: 'Trainspotting' });
+
+    assert.equal(finalState, expected);
+  });
 });
